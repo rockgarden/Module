4fb6ba7a6010bb5b0b8f898651d35b58479901e0
rockgarden <ro>
2017-10-13 15:53:50 +0800

    add

--- Material/build/Material.build/Release-iphoneos/Material.build/Objects-normal/arm64/Material_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/wangkan/Documents/Module/Material/build/Material.build/Release-iphoneos/Material.build/DerivedSources/Material_vers.c
--- Material/build/Material.build/Release-iphonesimulator/Material.build/Objects-normal/i386/Material_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/wangkan/Documents/Module/Material/build/Material.build/Release-iphonesimulator/Material.build/DerivedSources/Material_vers.c
